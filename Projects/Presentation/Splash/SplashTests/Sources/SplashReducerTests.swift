03ceaa512936f8551fccab7095fa7d25fbd75ae1
Roy <shuwj81@daum.net>
2025-10-27 12:09:55 +0900

    [test] 스플래쉬 회면  테스트 코드 작성 #1

---
+++ Projects/Presentation/Splash/SplashTests/Sources/SplashReducerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SplashReducerTests.swift
+//  SplashTests
+//
+//  Created by Wonji Suh  on 10/27/25.
+//
+
+import Testing
+
+struct SplashReducerTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
